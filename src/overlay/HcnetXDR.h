2164c05b0ad1382e6a67906e0b83c3f9eb7cf78d
shantanu-hashcash <shantanu@hashcashconsultants.com>
2024-02-29 07:10:05 +0000

    First Commit

---
+++ src/overlay/HcnetXDR.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "xdr/Hcnet-contract.h"
+#include "xdr/Hcnet-ledger-entries.h"
+#include "xdr/Hcnet-ledger.h"
+#include "xdr/Hcnet-overlay.h"
+#include "xdr/Hcnet-transaction.h"
+#include "xdr/Hcnet-types.h"
